f5a8924cb5679f90ac48bdb01a9cd1f6ad122465
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-03-22 08:41:36 +0000

    finalise near to ETH transfer test

--- eNear/test/eNear.test.js
+++ eNear/test/eNear.test.js
@@ -2,12 +2,35 @@ const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test
 const { expect } = require('chai');
 const { ZERO_ADDRESS } = constants;
 
+const { serialize } = require('rainbow-bridge-lib/rainbow/borsh.js');
+const { borshifyOutcomeProof } = require('rainbow-bridge-lib/rainbow/borshify-proof.js');
+
+const { toWei, fromWei, hexToBytes } = web3.utils;
+
+const {ethers} = require('ethers')
+
+const NearProverMock = artifacts.require('NearProverMock');
 const eNear = artifacts.require('eNearMock');
 const TransparentUpgradeableProxy = artifacts.require('TransparentUpgradeableProxyMock');
 
 const eNearABI = require('../artifacts/contracts/eNear.sol/eNear.json').abi
 
-contract('eNear bridging', function ([deployer, proxyAdmin, prover, eNearAdmin, alice, ...otherAccounts]) {
+const SCHEMA = {
+  'Unlock': {
+    kind: 'struct', fields: [
+      ['flag', 'u8'],
+      ['amount', 'u128'],
+      ['token', [20]],
+      ['recipient', [20]],
+    ]
+  }
+};
+
+const UNPAUSED_ALL = 0
+const PAUSED_FINALISE_FROM_NEAR = 1 << 0
+const PAUSED_XFER_TO_NEAR = 1 << 1
+
+contract('eNear bridging', function ([deployer, proxyAdmin, prover, eNearAdmin, alice, bob, ...otherAccounts]) {
 
   const name = 'eNear';
   const symbol = 'eNear';
@@ -15,6 +38,8 @@ contract('eNear bridging', function ([deployer, proxyAdmin, prover, eNearAdmin, 
   const ONE_HUNDRED_TOKENS = new BN('100').mul(new BN('10').pow(new BN('24')))
 
   beforeEach(async () => {
+    this.proverMock = await NearProverMock.new()
+
     this.logic = await eNear.new()
 
     // deploys the proxy and calls init on the implementation
@@ -24,8 +49,8 @@ contract('eNear bridging', function ([deployer, proxyAdmin, prover, eNearAdmin, 
       await new web3.eth.Contract(eNearABI).methods.init(
         name,
         symbol,
-        Buffer.from('factory', 'utf-8'),
-        prover,
+        Buffer.from('eNearBridge', 'utf-8'),
+        this.proverMock.address,
         eNearAdmin,
         0
       ).encodeABI(),
@@ -33,6 +58,22 @@ contract('eNear bridging', function ([deployer, proxyAdmin, prover, eNearAdmin, 
     )
 
     this.token = await eNear.at(this.proxy.address)
+  })
+
+  describe('init()', () => {
+    it('Reverts when version already initialised', async () => {
+      await expectRevert(
+        this.token.init(
+          name,
+          symbol,
+          Buffer.from('eNearBridge', 'utf-8'),
+          this.proverMock.address,
+          eNearAdmin,
+          0
+        ),
+        "Can only call init() once per version"
+      )
+    })
   })
 
   describe('transferToNear()', () => {
@@ -68,5 +109,26 @@ contract('eNear bridging', function ([deployer, proxyAdmin, prover, eNearAdmin, 
         }
       )
     })
+  })
+
+  describe('finaliseNearToEthTransfer()', () => {
+    it('Mints eNear after bridging Near', async () => {
+      let proof = require('./proof_template.json');
+
+      const amount = ethers.utils.parseUnits('1', '24');
+      proof.outcome_proof.outcome.status.SuccessValue = serialize(SCHEMA, 'Unlock', {
+        flag: 0,
+        amount: amount.toString(),
+        token: hexToBytes(this.token.address),
+        recipient: hexToBytes(bob),
+      }).toString('base64');
+
+      const receiverBalance = await token.balanceOf(bob);
+
+      await this.token.finaliseNearToEthTransfer(borshifyOutcomeProof(proof), 1099);
+
+      const newReceiverBalance = await this.token.balanceOf(bob);
+      expect(newReceiverBalance.sub(receiverBalance).toString()).to.be.equal(amount);
+    });
   })
 })
