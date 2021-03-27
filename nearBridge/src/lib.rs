f9fe61da22833af5bb57d4ea814c9f7cf5570a6c
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-03-27 22:08:02 +0000

    finish_eth_to_near_transfer()

--- nearBridge/src/lib.rs
+++ nearBridge/src/lib.rs
@@ -4,7 +4,9 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupMap, UnorderedSet};
 use near_sdk::json_types::U128;
-use near_sdk::{env, near_bindgen, AccountId, Balance, Promise, StorageUsage};
+use near_sdk::{
+    env, near_bindgen, AccountId, Balance, Promise, StorageUsage, ext_contract, Gas
+};
 
 #[global_allocator]
 static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc::INIT;
@@ -13,13 +15,22 @@ static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc:
 const STORAGE_PRICE_PER_BYTE: Balance = 100_000_000_000_000_000_000;
 
 type EthereumAddress = [u8; 20];
-
+pub use lock_event::TransferToNearInitiatedEvent;
 use prover::*;
 pub use prover::{validate_eth_address, Proof};
 
 mod lock_event;
 pub mod prover;
 mod unlock_event;
+
+/// Gas to call finalise method.
+const FINISH_FINALISE_GAS: Gas = 50_000_000_000_000;
+
+const NO_DEPOSIT: Balance = 0;
+
+
+/// Gas to call verify_log_entry on prover.
+const VERIFY_LOG_ENTRY_GAS: Gas = 50_000_000_000_000;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -77,6 +88,83 @@ impl NearBridge {
     pub fn finalise_eth_to_near_transfer(&mut self, #[serializer(borsh)] proof: Proof) {
         //self.check_not_paused(PAUSE_DEPOSIT);
 
+        let event = TransferToNearInitiatedEvent::from_log_entry_data(&proof.log_entry_data);
+        assert_eq!(
+            event.e_near_address,
+            self.e_near_address,
+            "Event's address {} does not match locker address of this token {}",
+            hex::encode(&event.e_near_address),
+            hex::encode(&self.e_near_address),
+        );
+
+        let proof_1 = proof.clone();
+
+        ext_prover::verify_log_entry(
+            proof.log_index,
+            proof.log_entry_data,
+            proof.receipt_index,
+            proof.receipt_data,
+            proof.header_data,
+            proof.proof,
+            false, // Do not skip bridge call. This is only used for development and diagnostics.
+            &self.prover_account,
+            NO_DEPOSIT,
+            VERIFY_LOG_ENTRY_GAS,
+        )
+            .then(ext_self::finish_eth_to_near_transfer(
+                event.recipient,
+                event.amount,
+                proof_1,
+                &env::current_account_id(),
+                env::attached_deposit(),
+                FINISH_FINALISE_GAS,
+            ));
+    }
+
+    /// Finish depositing once the proof was successfully validated. Can only be called by the contract
+    /// itself.
+    #[payable]
+    pub fn finish_eth_to_near_transfer(
+        &mut self,
+        #[callback]
+        #[serializer(borsh)]
+        verification_success: bool,
+        #[serializer(borsh)] new_owner_id: AccountId,
+        #[serializer(borsh)] amount: Balance,
+        #[serializer(borsh)] proof: Proof,
+    ) {
+        assert_self();
+        assert!(verification_success, "Failed to verify the proof");
+
+        let required_deposit = self.record_proof(&proof);
+
+        assert!(
+            env::attached_deposit() >= required_deposit
+        );
+
+        Promise::new(new_owner_id).transfer(amount);
+    }
+
+    /// Record proof to make sure it is not re-used later for anther deposit.
+    fn record_proof(&mut self, proof: &Proof) -> Balance {
+        // TODO: Instead of sending the full proof (clone only relevant parts of the Proof)
+        //       log_index / receipt_index / header_data
+        assert_self();
+        let initial_storage = env::storage_usage();
+        let mut data = proof.log_index.try_to_vec().unwrap();
+        data.extend(proof.receipt_index.try_to_vec().unwrap());
+        data.extend(proof.header_data.clone());
+        let key = env::sha256(&data);
+        assert!(
+            !self.used_events.contains(&key),
+            "Event cannot be reused for depositing."
+        );
+        self.used_events.insert(&key);
+        let current_storage = env::storage_usage();
+
+        let required_deposit =
+            Balance::from(current_storage - initial_storage) * STORAGE_PRICE_PER_BYTE;
+        required_deposit
     }
 }
 
@@ -104,4 +192,22 @@ impl NearBridge {
             Promise::new(env::predecessor_account_id()).transfer(refund_amount);
         }
     }
+}
+
+#[ext_contract(ext_self)]
+pub trait ExtNearBridge {
+    #[result_serializer(borsh)]
+    fn finish_eth_to_near_transfer(
+        &mut self,
+        #[callback]
+        #[serializer(borsh)]
+        verification_success: bool,
+        #[serializer(borsh)] new_owner_id: AccountId,
+        #[serializer(borsh)] amount: Balance,
+        #[serializer(borsh)] proof: Proof,
+    ) -> Promise;
+}
+
+pub fn assert_self() {
+    assert_eq!(env::predecessor_account_id(), env::current_account_id());
 }
