c375b2774c0a8b78ec471c2354f83a93cd782500
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-05-19 08:41:49 +0100

    min acceptance height

--- eNear/contracts/Bridge.sol
+++ eNear/contracts/Bridge.sol
@@ -28,12 +28,17 @@ contract Bridge {
     internal
     returns (ProofDecoder.ExecutionStatus memory result)
     {
-        require(proofBlockHeight >= minBlockAcceptanceHeight, "Proof is from the ancient block");
         require(prover.proveOutcome(proofData, proofBlockHeight), "Proof should be valid");
 
         // Unpack the proof and extract the execution outcome.
         Borsh.Data memory borshData = Borsh.from(proofData);
         ProofDecoder.FullOutcomeProof memory fullOutcomeProof = borshData.decodeFullOutcomeProof();
+
+        require(
+            fullOutcomeProof.block_header_lite.inner_lite.height >= minBlockAcceptanceHeight,
+            "Proof is from the ancient block"
+        );
+
         require(borshData.finished(), "Argument should be exact borsh serialization");
 
         bytes32 receiptId = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.receipt_ids[0];
