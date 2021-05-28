a0442f26d0d799953b3e35a6539d47e3b1c88e99
Marcelo <mfornet94@gmail.com>
2021-05-28 13:48:46 +0200

    Update hardhat config

--- eNear/hardhat.config.js
+++ eNear/hardhat.config.js
@@ -1,18 +1,43 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-etherscan");
 require('solidity-coverage');
 require('hardhat-gas-reporter');
 require('@nomiclabs/hardhat-solhint');
 require('./scripts/1_deploy_eNear');
 require('./scripts/2_pause_e_near_method');
 
+/**
+ * Read env variable looking for rpc endpoint to be used. Set this variable using:
+ *
+ * export MAINNET_RPC_ENDPOINT=...
+ * export ROPSTEN_RPC_ENDPOINT=...
+ *
+ * @param {string} network
+ * @returns {string[]} List of private keys to be used to deploy and submit.
+ */
 function rpc_endpoint(network) {
   return process.env[`${network.toUpperCase()}_RPC_ENDPOINT`] || '';
 }
 
-function private_key(network) {
-  return process.env[`${network.toUpperCase()}_PRIVATE_KEY`] || '0x0000000000000000000000000000000000000000000000000000000000000000'
+/**
+ * Read env variable looking for private keys to be used. Set this variable using:
+ *
+ * export MAINNET_PRIVATE_KEY=...
+ * export ROPSTEN_PRIVATE_KEY=...
+ *
+ * @param {string} network
+ * @returns {string[]} List of private keys to be used to deploy and submit.
+ */
+function accounts(network) {
+  let result = process.env[`${network.toUpperCase()}_PRIVATE_KEY`];
+  if (result === undefined) {
+    result = [];
+  } else {
+    result = [result];
+  }
+  return result;
 }
 
 module.exports = {
@@ -37,13 +62,16 @@ module.exports = {
       // ROPSTEN_RPC_ENDPOINT
       url: rpc_endpoint('ropsten'),
       // ROPSTEN_PRIVATE_KEY
-      accounts: [private_key('ropsten')]
+      accounts: accounts('ropsten')
     },
     mainnet: {
       // MAINNET_RPC_ENDPOINT
       url: rpc_endpoint('mainnet'),
       // MAINNET_PRIVATE_KEY
-      accounts: [private_key('mainnet')]
+      accounts: accounts('mainnet')
     }
+  },
+  etherscan: {
+    apiKey: process.env['ETHERSCAN_API_KEY'] || ''
   }
 };
